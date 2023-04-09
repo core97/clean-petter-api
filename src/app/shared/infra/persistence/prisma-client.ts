7bdcd2fe52db9f9b1f94733466b46d70ef83712c
Juan Cortes <j.cortes@crowdfarming.com>
2023-04-09 14:52:45 +0200

    feat: create middleware in prisma to validate entities

--- src/app/shared/infra/persistence/prisma-client.ts
+++ src/app/shared/infra/persistence/prisma-client.ts
@@ -1,9 +1,60 @@
-import { PrismaClient } from '@prisma/client';
+import PetAdValidator from '@pet-ad/application/pet-ad-validator';
+import UserValidator from '@user/application/user-validator';
+import VisitValidator from '@visit/application/visit-validator';
+import { PrismaClient, Prisma as PrismaModule } from '@prisma/client';
 
 export class Prisma {
   client: PrismaClient;
 
-  constructor() {
+  constructor(
+    private deps: {
+      petAdValidator: PetAdValidator;
+      userValidator: UserValidator;
+      visitValidator: VisitValidator;
+    }
+  ) {
     this.client = new PrismaClient();
+    this.setMiddlewares();
+  }
+
+  private setMiddlewares() {
+    this.setValidatorMiddleware();
+  }
+
+  private setValidatorMiddleware() {
+    const validate: {
+      [key in PrismaModule.ModelName]?: Function;
+    } = {
+      PetAd: this.deps.petAdValidator.validate,
+      User: this.deps.petAdValidator.validate,
+      Visit: this.deps.petAdValidator.validate,
+    };
+
+    this.client.$use(async (params, next) => {
+      if (
+        params.model &&
+        (params.action === 'create' ||
+          params.action === 'createMany' ||
+          params.action === 'update' ||
+          params.action === 'updateMany' ||
+          params.action === 'upsert')
+      ) {
+        const modelName = params.model;
+
+        if (Array.isArray(params.args.data)) {
+          await Promise.all(
+            params.args.data.map(async (data: any) => {
+              if (typeof params.args.data === 'object') {
+                await validate[modelName]?.(data);
+              }
+            })
+          );
+        } else if (typeof params.args.data === 'object') {
+          await validate[modelName]?.(params.args.data);
+        }
+      }
+
+      return next(params);
+    });
   }
 }
